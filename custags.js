4ac9dbe91b68bca781a34598f9b717d022039548
obnoxiousnerd <52596591+obnoxiousnerd@users.noreply.github.com>
2019-09-30 20:29:32 +0530

    Fixed major bug issue #2

--- custags.js
+++ custags.js
@@ -1,161 +1,113 @@
-class button extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({mode: 'open'});
-        const button = document.createElement('button');
-        button.style.cssText = 'display:block';
-        button.textContent = super.textContent;
-        shadow.appendChild(button);
-    }
-}
-class inputText extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.style.cssText = 'display:block';
-        button.textContent = super.textContent;
-        shadow.appendChild(input);
-    }
-}
-class inputCheckbox extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const input = document.createElement('input');
-        input.type = 'checkbox';
-        input.textContent = super.textContent;
-        shadow.appendChild(input);
-    }
-}
-class inputColor extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const input = document.createElement('input');
-        input.type = 'color';
-        input.textContent = super.textContent;
-        shadow.appendChild(input);
-    }
-}
-class inputEmail extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const input = document.createElement('input');
-        input.type = 'email';
-        input.textContent = super.textContent;
-        shadow.appendChild(input);
-    }
-}
-class div extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const div = document.createElement('div');
-        div.textContent = super.textContent;
-        shadow.appendChild(div);
-    }
-}
-class textarea extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const textarea = document.createElement('textarea');
-        textarea.textContent = super.textContent;
-        shadow.appendChild(textarea);
-    }
-}
-class h1 extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const h1 = document.createElement('h1');
-        h1.textContent = super.textContent;
-        shadow.appendChild(h1);
-    }
-}
-class code extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const code = document.createElement('code');
-        code.textContent = super.textContent;
-        shadow.appendChild(code);
-    }
-}
-class h2 extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const h2 = document.createElement('h2');
-        h2.textContent = super.textContent;
-        shadow.appendChild(h2);
-    }
-}
-class h3 extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const h3 = document.createElement('h3');
-        h3.textContent = super.textContent;
-        shadow.appendChild(h3);
-    }
-}
-class h4 extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const h4 = document.createElement('h4');
-        h4.textContent = super.textContent;
-        shadow.appendChild(h4);
-    }
-}
-class h5 extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const h5 = document.createElement('h5');
-        h5.textContent = super.textContent;
-        shadow.appendChild(h5);
-    }
-}
-class h6 extends HTMLElement{
-    constructor(){
-        super();
-        const shadow = this.attachShadow({
-            mode: 'open'
-          });
-        const h6 = document.createElement('h6');
-        h6.textContent = super.textContent;
-        shadow.appendChild(h6);
-    }
-}
+const textarea = (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const textarea = document.createElement('textarea');
+          textarea.innerHTML = ptag.textContent;
+          shadow.appendChild(textarea);
+      }
+const pre = (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const pre = document.createElement('pre');
+          pre.innerHTML = ptag.textContent;
+          shadow.appendChild(pre);
+      }
+const a = (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const a = document.createElement('a');
+          a.innerHTML = ptag.textContent;
+          shadow.appendChild(a);
+      }
+const p= (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const p = document.createElement('p');
+          p.innerHTML = ptag.textContent;
+          shadow.appendChild(p);
+      }
+const button= (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const button = document.createElement('button');
+          button.innerHTML = ptag.textContent;
+          shadow.appendChild(button);
+      }
+const code= (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const code = document.createElement('code');
+          code.innerHTML = ptag.textContent;
+          shadow.appendChild(code);
+      }
+const inputText = (tag) => {
+        const ptag = document.querySelector(tag);
+              const shadow = ptag.attachShadow({
+                  mode: 'open'
+                });
+              const input = document.createElement('input');
+              input.type='text';
+              input.innerHTML = ptag.textContent;
+              shadow.appendChild(input);
+        }
+const h1= (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const h1 = document.createElement('h1');
+          h1.innerHTML = ptag.textContent;
+          shadow.appendChild(h1);
+      }
+  const h2= (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const h2 = document.createElement('h2');
+          h2.innerHTML = ptag.textContent;
+          shadow.appendChild(h2);
+      }
+  const h3= (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const h3 = document.createElement('h3');
+          h3.innerHTML = ptag.textContent;
+          shadow.appendChild(h3);
+      }
+  const h4= (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const h4 = document.createElement('h4');
+          h4.innerHTML = ptag.textContent;
+          shadow.appendChild(h4);
+      }
+  const h5= (tag) => {
+    const ptag = document.querySelector(tag);
+          const shadow = ptag.attachShadow({
+              mode: 'open'
+            });
+          const h5 = document.createElement('h5');
+          h5.innerHTML = ptag.textContent;
+          shadow.appendChild(h5);
+      }
+
 function Ω(selector){
     var self = {};
     self.selector = selector;
@@ -165,18 +117,33 @@ function Ω(selector){
         window.onload = fn;
         return fn;
     }
-
-    self.register = function(className){
-        customElements.define(selector, className);
+    self.register = (val)=>{
+       val(selector);   
+    }
+    self.h1 = ()=>{
+        h1(selector);
+    }
+    self.h5 = ()=>{
+        h5(selector);
     }
     self.css = function(style){
         const el = document.querySelector(selector);
         el.style.cssText = style;
     }
     self.value = () => {
-        const el = document.querySelector(selector).textContent;
+        const el = document.querySelector(selector).value;
         console.log(el);
-        return el;
+    }
+    self.extends = (tag) => {
+        
+    }
+    self.ready = (fn) => {
+        if (document.readyState === 'complete') {
+            // The page is fully loaded
+            eval(fn);
+            
+            
+          }
     }
     return self;
 }
