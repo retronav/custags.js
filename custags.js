e1791b5b30c8a6c525a1300320ac928ccf6daed6
obnoxiousnerd <52596591+obnoxiousnerd@users.noreply.github.com>
2019-10-03 16:22:14 +0530

    all elems added

--- custags.js
+++ custags.js
@@ -3,7 +3,7 @@ const extendCSS = (el1, el2) =>{
     el2.setAttribute(item.name, item.value);
   });
 }
-//main tag builder
+/*main tag builder*/
 const build = (elementType) => tag => {
   const query = document.querySelectorAll(tag);
   query.forEach(ptag => {
@@ -17,7 +17,7 @@ const build = (elementType) => tag => {
     shadow.host.parentNode.replaceChild(element, shadow.host);
   });
 };
-//input tag builder
+/*input tag builder*/
 const inputBuild = (type) => tag => {
   const query = document.querySelectorAll(tag);
   query.forEach(ptag => {
@@ -32,7 +32,7 @@ const inputBuild = (type) => tag => {
     shadow.host.parentNode.replaceChild(element, shadow.host);
   });
 };
-//tag definers
+/*tag definers*/
 const h1 = build('h1');
 const h2 = build('h2');
 const h3 = build('h3');
@@ -56,12 +56,101 @@ const article = build('article');
 const button = build('button');
 const textarea = build('textarea');
 const meter = build('meter');
+const abbr = build('abbr');
+const address = build('address');
+const bdi = build('bdi');
+const bdo = build('bdo');
+const blockquote = build('blockquote');
+const cite = build('cite');
+const code = build('code');
+const del = build('del');
+const dfn = build('dfn');
+const em = build('em');
+const ins = build('ins');
+const kbd = build('kbd');
+const mark = build('mark');
+const progress = build('progress');
+const q = build('q');
+const rp = build('rp');
+const rt = build('rt');
+const ruby = build('ruby');
+const s = build('s');
+const samp = build('samp');
+const small = build('small');
+const sub = build('sub');
+const sup = build('sup');
+const template = build('template');
+const time = build('time');
+const u = build('u');
+const variable = build('var');
+const wbr = build('wbr');
 const div = build('div');
+const form = build('form');
+const select = build('select');
+const optgroup = build('optgroup');
+const option = build('option');
+const label = build('label');
+const fieldset = build('fieldset');
+const legend = build('legend');
+const datalist = build('datalist');
+const output = build('output');
+const iframe = build('iframe');
+const map = build('map');
+const area = build('area');
+const canvas = build('canvas');
+const figcaption = build('figcaption');
+const figure = build('figure');
+const picture = build('picture');
+const svg = build('svg');
+const source = build('source');
+const track = build('track');
+const nav = build('nav');
+const ul = build('ul');
+const ol = build('ol');
+const li = build('li');
+const dl = build('dl');
+const dt = build('dt');
+const dd = build('dd');
+const table = build('table');
+const caption = build('caption');
+const th = build('th');
+const tr = build('tr');
+const td = build('td');
+const thead = build('thead');
+const tbody = build('tbody');
+const tfoot = build('tfoot');
+const col = build('col');
+const colgroup = build('colgroup');
+const main = build('main');
+const aside = build('aside');
+const details = build('details');
+const dialog = build('dialog');
+const summary = build('summary');
+const data = build('data');
+const embed = build('embed');
+const object = build('object');
+const param = build('param');
 const inputText = inputBuild('text');
 const inputColor = inputBuild('color');
 const inputSearch = inputBuild('search');
 const inputSubmit = inputBuild('submit');
 const inputRange = inputBuild('range');
+const inputButton = inputBuild('button');
+const inputCheckbox = inputBuild('checkbox');
+const inputDate = inputBuild('date');
+const inputDatetTimeLocal = inputBuild('datetime-local');
+const inputEmail = inputBuild('email');
+const inputFile = inputBuild('file');
+const inputHidden = inputBuild('hidden');
+const inputImage = inputBuild('image');
+const inputMonth = inputBuild('month');
+const inputNumber = inputBuild('number');
+const inputRadio = inputBuild('radio');
+const inputReset = inputBuild('reset');
+const inputTel = inputBuild('tel');
+const inputTime = inputBuild('time');
+const inputURL = inputBuild('url');
+const inputWeek = inputBuild('week');
 function Ω(selector){
     var self = {};
     self.selector = selector;
@@ -79,7 +168,11 @@ function Ω(selector){
     }
     self.value = () => {
         const el = document.querySelector(selector).value;
-        console.log(el);
+        return el;
+    }
+    self.html = () =>{
+      const el = document.querySelector(selector).innerHTML;
+        return el;
     }
     self.detectAdBlock = (mode) => {
       var adBlockEnabled = false;
@@ -134,12 +227,14 @@ function Ω(selector){
     }
     self.ready = (callback) => { 
     if (selector.readyState!='loading') callback();
-    // modern browsers
+    /* modern browsers*/
     else if (selector.addEventListener) selector.addEventListener('DOMContentLoaded', callback);
-    // IE <= 8
+    /* IE <= 8*/
     else selector.attachEvent('onreadystatechange', function(){
         if (selector.readyState=='complete') callback();
     });
     }
     return self;
 }
+
+
