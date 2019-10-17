2819ebac3c10551b709c19d82b9a7b5e21deee96
obnoxiousnerd <pdkarawale31@gmail.com>
2019-10-17 17:12:38 +0530

    add website

---
+++ docs/js/main.js
@@ -0,0 +1,9 @@
+Ω('topic-name').register(h1);
+Ω('strong-text').register(h1);
+Ω('sub-tpoic').register(p);
+Ω('main-wrap').register(div);
+new fullpage('#main-wrap', {
+   autoScrolling: true,
+   navigation: true,
+   controlArrows: true
+});
--- docs/main.js
+++
@@ -1,2 +0,0 @@
-Ω('topic-name').register(h1);
-Ω('main-wrap').register(div);
